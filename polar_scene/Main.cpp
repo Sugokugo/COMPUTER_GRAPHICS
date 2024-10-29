b3800b0a842c3079819a0c4a0b339027dc96274c
Sugokugo <kosurisaiakhil@gmail.com>
2024-10-29 19:55:14 +0000

    Window with color

--- polar_scene/Main.cpp
+++ polar_scene/Main.cpp
@@ -3,5 +3,36 @@
 #include<GLFW/glfw3.h>
 
 int main() {
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "scene", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "Failed error" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 800);
+
+	glClearColor(0.0f, 0.41f, 0.58f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
 	return 0;
 }
