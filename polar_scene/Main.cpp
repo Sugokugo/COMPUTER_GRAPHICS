76e7149fa4a10e4a9ddffe94276542ea0615d30e
Sugokugo <kosurisaiakhil@gmail.com>
2024-10-29 22:32:39 +0000

    Increased triangle vertices

--- polar_scene/Main.cpp
+++ polar_scene/Main.cpp
@@ -29,6 +29,16 @@ int main() {
 		-0.5f,-0.5f * float(sqrt(3)) / 3,0.0f,
 		0.5f,-0.5f * float(sqrt(3)) / 3,0.0f,
 		0.0f,0.5f * float(sqrt(3)) * 2 / 3,0.0f,
+
+		-0.5f/2,0.5f * float(sqrt(3)) / 6,0.0f,
+		0.5f/2,0.5f * float(sqrt(3)) / 6,0.0f,
+		0.0f,-0.5f * float(sqrt(3)) / 3,0.0f,
+	};
+
+	GLuint indices[] = {
+		0,3,5,
+		3,2,4,
+		5,4,1
 	};
 
 	//window dimension
@@ -63,18 +73,24 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
 	glClearColor(0.0f, 0.41f, 0.58f, 1.0f);
@@ -86,7 +102,7 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
@@ -94,6 +110,7 @@ int main() {
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	glfwDestroyWindow(window);
